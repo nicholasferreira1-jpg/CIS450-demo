f8df843b078d54590d6007cf4574f88f5aa840f4
nicholasferreira1-jpg <nferreira@bushnell.edu>
2026-09-22 17:26:51 -0700

    Made changes so panorama can work

--- panorama.py
+++ panorama.py
@@ -1,33 +1,27 @@
 from __future__ import print_function
-
+ 
 import numpy as np
 import cv2 as cv
-
+ 
 import argparse
 import sys
-import re
-
+ 
 modes = (cv.Stitcher_PANORAMA, cv.Stitcher_SCANS)
-
+ 
 parser = argparse.ArgumentParser(prog='stitching.py', description='Stitching sample.')
 parser.add_argument('--mode',
     type = int, choices = modes, default = cv.Stitcher_PANORAMA,
     help = 'Determines configuration of stitcher. The default is `PANORAMA` (%d), '
-         'mode suitable for creating photo panoramas. Option `SCANS` (%d) is suitable '
-         'for stitching materials under affine transformation, such as scans.' % modes)
+        'mode suitable for creating photo panoramas. Option `SCANS` (%d) is suitable '
+        'for stitching materials under affine transformation, such as scans.' % modes)
 parser.add_argument('--output', default = 'result.jpg',
     help = 'Resulting image. The default is `result.jpg`.')
 parser.add_argument('img', nargs='+', help = 'input images')
 
-__doc__ = (__doc__ or '') + '\n' + parser.format_help()
-
-def natural_key(s):
-    return [int(t) if t.isdigit() else t for t in re.split(r'(\d+)', s)]
-
+  
 def main():
     args = parser.parse_args()
-    args.img.sort(key=natural_key)
-
+ 
     # read input images
     imgs = []
     for img_name in args.img:
@@ -36,21 +30,21 @@ def main():
             print("can't read image " + img_name)
             sys.exit(-1)
         imgs.append(img)
-
+ 
     #![stitching]
     stitcher = cv.Stitcher.create(args.mode)
     status, pano = stitcher.stitch(imgs)
-
+ 
     if status != cv.Stitcher_OK:
         print("Can't stitch images, error code = %d" % status)
         sys.exit(-1)
     #![stitching]
-
+ 
     cv.imwrite(args.output, pano)
     print("stitching completed successfully. %s saved!" % args.output)
-
+ 
     print('Done')
-
+ 
 if __name__ == '__main__':
     print(__doc__)
     main()
